fcbdef90ed17471ee7dc1d75c422253906870486
grimaldo.soto <Grimaldo.SOTO@icloud.com>
2023-08-02 20:30:30 -0500

    Tarea y Resolución del Ejercicio #1

--- udemy-fernando-herrera.com-typescript/bases/app.js
+++ udemy-fernando-herrera.com-typescript/bases/app.js
@@ -1,5 +1,36 @@
 "use strict";
 (() => {
-    const a = 10;
-    console.log(a);
+    // Tipos
+    const batman = 'Bruce';
+    const superman = 'Clark';
+    const existe = false;
+    // Tuplas
+    const parejaHeroes = [batman, superman];
+    const villano = ['Lex Lutor', 5, true];
+    //Arreglos
+    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    // ? enum {acuaman = 0}
+    let Power;
+    (function (Power) {
+        Power[Power["Flash"] = 5] = "Flash";
+        Power[Power["Superman"] = 100] = "Superman";
+        Power[Power["Batman"] = 1] = "Batman";
+        Power[Power["Acuaman"] = 0] = "Acuaman";
+    })(Power || (Power = {}));
+    const fuezaFlash = Power.Flash;
+    const fuerzaSuperman = Power.Superman;
+    const fuerzaBatman = Power.Batman;
+    const fuerzaAcuaman = Power.Acuaman;
+    //Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
+    }
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    // Aserciones de Tipo
+    const poder = '100';
+    const largoDelPoder = poder.length;
+    console.log(largoDelPoder);
 })();
--- udemy-fernando-herrera.com-typescript/bases/app.ts
+++ udemy-fernando-herrera.com-typescript/bases/app.ts
@@ -1,7 +1,47 @@
-(() => {
-     
-    const a: number = 10;
+(()=>{
 
-    console.log(a);
+    // Tipos
+    const batman: string = 'Bruce';
+    const superman: string = 'Clark';
+
+    const existe: boolean = false;
+
+
+    // Tuplas
+    const parejaHeroes: [string, string] = [batman,superman];
+    const villano:[string, number, boolean] =['Lex Lutor', 5, true];
+
+    //Arreglos
+    const aliados: string[]= ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+
+    //Enumeraciones
+    // ? enum {acuaman = 0}
+    
+    enum Power{
+        Flash = 5,
+        Superman = 100,
+        Batman = 1,
+        Acuaman = 0
+    }
+    const fuezaFlash:Power = Power.Flash;
+    const fuerzaSuperman:Power = Power.Superman;
+    const fuerzaBatman:Power = Power.Batman;
+    const fuerzaAcuaman:Power = Power.Acuaman;
+    
+    //Retorno de funciones
+    function activar_batiseñal(): string{
+        return 'activada';
+    }
+
+    function pedir_ayuda(): void{
+        console.log('Auxilio!!!');
+    }
+
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = (poder as string).length;
+    console.log(largoDelPoder);
+
+
 
 })()
