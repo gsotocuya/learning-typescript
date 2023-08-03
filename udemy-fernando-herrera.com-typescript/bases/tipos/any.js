c53e7d0269e4a344a5420b3e2d5483e760643ab1
grimaldo.soto <Grimaldo.SOTO@icloud.com>
2023-08-02 19:11:45 -0500

    Tipo Any

---
+++ udemy-fernando-herrera.com-typescript/bases/tipos/any.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let avenger = 123;
+    let exists;
+    let power;
+    avenger = 'Dr Strange';
+    // console.log(avenger.charAt(0));
+    console.log(avenger.charAt(0));
+    avenger = 124.33333;
+    console.log(avenger.toFixed(3));
+    console.log(exists);
+    console.log(power);
+})();
