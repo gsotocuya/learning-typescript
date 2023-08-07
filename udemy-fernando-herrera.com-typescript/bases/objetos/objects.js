7a70bf28a46fd2fc264f76588487308ebfbad813
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-06 20:25:15 -0500

    Objetos básicos

---
+++ udemy-fernando-herrera.com-typescript/bases/objetos/objects.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Súper velocidad', 'Viajar en el tiempo']
+    };
+    flash = {
+        name: 'Clark kent',
+        age: 60,
+        powers: ['Súper fueza'],
+        getNombre() {
+            return this.name;
+        }
+    };
+})();
