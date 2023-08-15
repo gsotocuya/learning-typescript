9db3a6aff4b386483bfa6c0c20e09ea5ce839905
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-14 19:58:45 -0500

    Variables LET

---
+++ udemy-fernando-herrera.com-typescript/bases/ES6/let-var.ts
@@ -0,0 +1,14 @@
+(() => {
+
+  // console.log(a);
+
+  const nombre:string = 'Fernando';
+
+  const getName = ():void => {
+    console.log('viejo getname')
+  }
+
+  // getName = () => { console.log('Nuevo getName')};
+  // getName()
+
+})()
