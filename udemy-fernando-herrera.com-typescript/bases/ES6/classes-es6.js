35e48c264eab22de7db7213a18746d8e6b2c17c2
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-14 20:46:11 -0500

    Clases en ES6

---
+++ udemy-fernando-herrera.com-typescript/bases/ES6/classes-es6.js
@@ -0,0 +1,24 @@
+(()=>{
+  class Avenger {
+    constructor(name = 'No name', power = 0){
+      this.name = name;
+      this.power = power;
+    }
+  }
+
+
+  class FlyingAvenger extends Avenger{
+    constructor(name, power){
+      super(name, power);
+      this.flying = true;
+    }
+  }
+
+  const hulk = new Avenger('hulk', 9001);
+  const falcon = new FlyingAvenger('falcon', 50);
+
+
+  console.log(hulk);
+  console.log(falcon);
+
+})()
