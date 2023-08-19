7ee9e571a62e2c3178e2ae8d10c66668917a4b6b
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-18 20:06:09 -0500

    Definición de una clase básica en TypeScript

---
+++ udemy-fernando-herrera.com-typescript/bases/classes/basica.ts
@@ -0,0 +1,19 @@
+(() => {
+    class Avenger {
+        private name: string;
+        private team: string;
+        public realName?: string;
+        static avgAge: number = 35;
+        constructor(name: string, team: string, realName?: string) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
+        }
+
+    }
+
+    const antman:Avenger = new Avenger('AntMan', 'Capitan');
+    console.log(antman);
+    //console.log(Avenger.avgAge)
+
+})()
--- udemy-fernando-herrera.com-typescript/bases/main.js
+++ udemy-fernando-herrera.com-typescript/bases/main.js
@@ -1,52 +1,14 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+(() => {
+    class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
         }
-    return t;
-};
-(() => {
-    const avengers = {
-        nick: 'Samuel L. Jackson',
-        ironman: 'Robert ',
-        vision: 'paul bettany',
-        activo: true,
-        poder: 1500.2121313
-    };
-    const printAvenger = (_a) => {
-        var { ironman } = _a, resto = __rest(_a, ["ironman"]);
-        console.log(ironman, resto);
-    };
-    const avengerArr = ['Cap. América', true, 1212.2121];
-    const [capitan, ironman, seriaUnNumero] = avengerArr;
-})();
-(() => {
-    const ironman = {
-        name: "ironman",
-        weapon: "Armorsuit",
-    };
-    const capitanAmerica = {
-        name: "capitan america",
-        weapon: "escudo",
-    };
-    const thor = {
-        name: "thor",
-        weapon: "mjolnir",
-    };
-    const avengers = [ironman, thor, capitanAmerica];
-    for (const avenger of avengers) {
-        console.log(avenger.name, avenger.weapon);
     }
-})();
-(() => {
-    const nombre = 'Fernando';
-    const getName = () => {
-        console.log('viejo getname');
-    };
+    Avenger.avgAge = 35;
+    const antman = new Avenger('AntMan', 'Capitan');
+    console.log(antman);
 })();
 //# sourceMappingURL=main.js.map
