772c5789cdce8c1cca71d8a9a5a246e2097b52d6
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-20 10:26:59 -0500

    Export default y exportación con alias

--- udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
+++ udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
@@ -1,10 +1,16 @@
-import {Hero} from "./classes/Hero";
+// import {Hero as SuperHero, Hero2} from "./classes/Hero";
+import * as HeroClasses from './classes/Hero';
+import powers, {Power}  from "./data/powers";
 
+const Hero = 123;
 
-const ironman = new Hero('Ironman', 1, 55)
+// const ironman = new SuperHero('Ironman', 1, 55)
+const ironman = new HeroClasses.Hero('Ironman', 1, 55)
+
 
 console.log(ironman);
 
+console.log(powers);
 
 
 
