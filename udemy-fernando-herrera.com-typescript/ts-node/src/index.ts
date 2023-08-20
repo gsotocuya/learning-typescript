78f4629ae46de98b94a12eea1f07e84f6961c061
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-20 10:44:36 -0500

    Tarea - resolver errores en TypeScript

--- udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
+++ udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
@@ -1,16 +1,13 @@
+import {Hero} from "./classes/Hero";
 // import {Hero as SuperHero, Hero2} from "./classes/Hero";
-import * as HeroClasses from './classes/Hero';
-import powers, {Power}  from "./data/powers";
-
-const Hero = 123;
+//import * as HeroClasses from './classes/Hero';
 
 // const ironman = new SuperHero('Ironman', 1, 55)
-const ironman = new HeroClasses.Hero('Ironman', 1, 55)
-
+const ironman = new Hero('Ironman', 10, 55)
 
 console.log(ironman);
+console.log(ironman.power);
 
-console.log(powers);
 
 
 
