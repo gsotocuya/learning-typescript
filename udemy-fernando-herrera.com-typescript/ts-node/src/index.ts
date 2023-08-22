313d67749020eb5a7a3dcad4399949a4e0023324
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-21 20:57:38 -0500

    funciones genéricas

--- udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
+++ udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
@@ -1,4 +1,4 @@
-import {genericFunction, printObject} from "./generics/generics";
+import {genericFunction, genericFunctionArrow, printObject} from "./generics/generics";
 
 // printObject(123);
 // printObject(new Date());
@@ -6,7 +6,8 @@ import {genericFunction, printObject} from "./generics/generics";
 // printObject([1,2,3,4,5,6]);
 // printObject(':)');
 
+const name:string = 'Grimaldo';
 
-console.log(genericFunction(3.1416).toFixed(2))
-console.log(genericFunction(new Date()).toFixed(2))
-// console.log(genericFunction(':D').toFixed(2))
+console.log(genericFunctionArrow(3.1416).toFixed(2))
+console.log(genericFunctionArrow(new Date()).getDate())
+console.log(genericFunctionArrow(name).toUpperCase())
