fb8b73d31711392eece2dc6efbe946efe28f1de3
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-23 20:31:14 -0500

    Mapear respuestas http

--- udemy-fernando-herrera.com-typescript/ts-node/src/generics/get-pokemons.ts
+++ udemy-fernando-herrera.com-typescript/ts-node/src/generics/get-pokemons.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import {Pokemon} from "../interfaces";
 
-export const getPokemons = async (pokemonName:string)=>{
-    const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-    console.log(resp)
-    return 1;
+export const getPokemons = async (pokemonName:string):Promise<Pokemon>=>{
+    const resp = await axios.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    // console.log(data);
+    return resp.data;
 }
--- udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
+++ udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
@@ -1,6 +1,6 @@
 import {getPokemons} from "./generics/get-pokemons";
 
 getPokemons('pikachu')
-    .then(resp => console.log(resp))
+    .then(pokemon => console.log(pokemon.sprites.front_default))
     .catch(error => console.log(error))
     .finally(() =>console.log('Fin de getPokemon'));
