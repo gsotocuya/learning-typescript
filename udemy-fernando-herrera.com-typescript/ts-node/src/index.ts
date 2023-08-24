d69432b5bf0db193043b761f2b92d7afc816de70
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-23 19:37:48 -0500

    Ejemplo de función genérica en acción

--- udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
+++ udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
@@ -1,4 +1,6 @@
 import {genericFunction, genericFunctionArrow, printObject} from "./generics/generics";
+import {Hero} from "./interfaces/hero";
+import {Villain} from "./interfaces/villain";
 
 // printObject(123);
 // printObject(new Date());
@@ -6,8 +8,15 @@ import {genericFunction, genericFunctionArrow, printObject} from "./generics/gen
 // printObject([1,2,3,4,5,6]);
 // printObject(':)');
 
-const name:string = 'Grimaldo';
+// const name:string = 'Grimaldo';
+//
+// console.log(genericFunctionArrow(3.1416).toFixed(2))
+// console.log(genericFunctionArrow(new Date()).getDate())
+// console.log(genericFunctionArrow(name).toUpperCase())
 
-console.log(genericFunctionArrow(3.1416).toFixed(2))
-console.log(genericFunctionArrow(new Date()).getDate())
-console.log(genericFunctionArrow(name).toUpperCase())
+const deadpool = {
+    name: 'deadpool',
+    realName: 'Wade Windston Wilson',
+    dangerLevel:130
+}
+console.log(genericFunctionArrow<Villain>(deadpool).dangerLevel);
