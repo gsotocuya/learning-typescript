853de77465b5b2141b48bac60b5f10f6471c1769
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-24 19:24:04 -0500

    Decoradores de métodos

--- udemy-fernando-herrera.com-typescript/ts-node/src/decorators/pokemon-class.ts
+++ udemy-fernando-herrera.com-typescript/ts-node/src/decorators/pokemon-class.ts
@@ -14,6 +14,20 @@ const bloquearPrototipo = function(constructor: Function){
     Object.seal(constructor.prototype);
 
 }
+function CheckValidaPokemonId(){
+    return function(target:any, propertyKey:string, descriptor: PropertyDescriptor){
+        const originalMethod = descriptor.value;
+        descriptor.value = (id:number) =>{
+            if(id < 1 || id > 800){
+                return console.error('EL id del pokemon debe estar entre 1 y 800');
+            }else{
+                return originalMethod(id);
+            }
+        }
+        // descriptor.value = () => console.log('Hola mundo');
+    }
+}
+
 @bloquearPrototipo
 @printToConsoleConditional(true)
 export class Pokemon{
@@ -21,5 +35,9 @@ export class Pokemon{
     constructor(
         public name:string
     ) {
+    }
+    @CheckValidaPokemonId()
+    savePokemonToDB(id: number){
+        console.log(`Pokemon guardado  en DB ${id}`);
     }
 }
--- udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
+++ udemy-fernando-herrera.com-typescript/ts-node/src/index.ts
@@ -1,5 +1,6 @@
 import {Pokemon} from "./decorators/pokemon-class";
 
 const charmander = new Pokemon('Charmander');
-(Pokemon.prototype as any).custonName = 'Pikachu';
-console.log(charmander);
+// (Pokemon.prototype as any).custonName = 'Pikachu';
+// console.log(charmander.savePokemonToDB(50));
+charmander.savePokemonToDB(3)
